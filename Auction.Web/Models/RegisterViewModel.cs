efc7be78efb30bd83450e9ab4a6cd6cadd640cd1
Melisa Doko <mdoko@helius-systems.com>
2024-10-14 11:08:48 +0200

    small fixes

--- Auction.Web/Models/RegisterViewModel.cs
+++ Auction.Web/Models/RegisterViewModel.cs
@@ -16,12 +16,15 @@
         [Required(ErrorMessage = "Confirm Password is required!")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "First Name is required!")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required!")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
